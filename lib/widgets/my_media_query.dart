6d0328bec3fddff8badf293a9e1413a75210d314
mike123789-dev <mike123789@g.skku.edu>
2021-03-27 22:48:30 +0900

    feat: update MediaQuery widget

--- lib/widgets/my_media_query.dart
+++ lib/widgets/my_media_query.dart
@@ -3,19 +3,33 @@ import 'package:flutter/material.dart';
 class MyMediaQuery extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    var screenSize = MediaQuery.of(context).size;
+    final data = MediaQuery.of(context);
+    final screenSize = data.size;
+    final orientation = data.orientation;
+    final padding = data.padding;
+    final insets = data.viewInsets;
+
     return Column(
       mainAxisAlignment: MainAxisAlignment.center,
       children: [
         Text(
           "width: ${screenSize.width}\nheight: ${screenSize.height}\naspect ratio: ${screenSize.aspectRatio}",
         ),
+        Text(
+          "orientation: ${orientation.toString()}\n",
+        ),
+        Text(
+          "top padding: ${padding.top}\nbottom padding: ${padding.bottom}\n",
+        ),
+        Text(
+          "top insets: ${insets.top}\nbottom insets: ${insets.bottom}\n",
+        ),
         SizedBox(
           height: 30,
         ),
         Container(
-          height: 500,
-          width: 500 * screenSize.aspectRatio,
+          height: 300,
+          width: 300 * screenSize.aspectRatio,
           color: Colors.blueGrey[100],
         )
       ],
